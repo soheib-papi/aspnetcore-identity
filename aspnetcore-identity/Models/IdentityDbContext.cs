05cc088b9a926726142877a56c1cd698810f1c79
Papi, Soheib <un.sohaib@gmail.com>
2025-02-09 10:06:17 +0330

    Add connection string to appsettings.Development.json. Implement OnConfiguring. Add Init migration.

--- aspnetcore-identity/Models/IdentityDbContext.cs
+++ aspnetcore-identity/Models/IdentityDbContext.cs
@@ -1,9 +1,23 @@
 ﻿using aspnetcore_identity.Models.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace aspnetcore_identity.Models;
 
 public class IdentityDatabaseContext: IdentityDbContext<UserIdentity, RoleIdentity, long>
 {
-    
+    private readonly IConfiguration _configuration;
+
+    public IdentityDatabaseContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
 }
