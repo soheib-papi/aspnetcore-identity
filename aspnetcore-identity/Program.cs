f96db61ada8ffecfa6739278fc7b55fa985cccdc
Papi, Soheib <un.sohaib@gmail.com>
2025-02-09 10:56:13 +0330

    Install Swashbuckle.AspNetCore package. Change launchSettings.json. Config Swagger in Program.cs.

--- aspnetcore-identity/Program.cs
+++ aspnetcore-identity/Program.cs
@@ -9,19 +9,24 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 var configuration = builder.Configuration;
-builder.Services.AddOpenApi();
 
 var connectionString = configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<IdentityDatabaseContext>(p =>
     p.UseSqlServer(connectionString));
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.MapControllers();
 
 app.UseHttpsRedirection();
 
